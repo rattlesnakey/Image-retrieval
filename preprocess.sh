7d012ff5d9c9970c05d9153a300e4fd0d91538d0
rattlesnakey <hengyuan_blcu@yeah.net>
2022-12-23 21:27:50 +0800

    Update

--- preprocess.sh
+++
@@ -1,4 +0,0 @@
-DATAPATH=../data/Multimodal_Retrieval/
-python src/preprocess/transform_images.py \
-    --data_dir ${DATAPATH} \
-    --image_resolution 224
--- transform_model.sh
+++
@@ -1,4 +0,0 @@
-PRETRAIN_MODEL_PATH=../pretrain_models/ViT-B-16
-python src/preprocess/transform_openai_pretrain_weights.py \
-    --raw-ckpt-path ${PRETRAIN_MODEL_PATH}/ViT-B-16.pt \
-    --new-ckpt-path ${PRETRAIN_MODEL_PATH}/ViT-B-16.state_dict.pt
